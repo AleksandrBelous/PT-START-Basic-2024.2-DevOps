3de52edfb3e44dd22ebfc9a4715af00dfc09d09e
AleksandrBelous <ab21082001@mail.ru>
2024-09-09 22:01:21 +0300

    -

--- task-1-3/data_preparation.py
+++ task-1-3/data_preparation.py
@@ -1,3 +1,4 @@
+import os
 import random
 import re
 import logging
@@ -10,38 +11,58 @@ logging.basicConfig(filename=f'task-1-3-log-{datetime.datetime.now()}.txt', leve
 
 
 def create_err_file(filename: str):
+    logging.debug(f"Начало {create_err_file.__name__}()")
     separators = [",", ";", "."]
     with open(filename, "w") as f:
+        # создадим файл на 10000 записей
         for _ in range(10000):
+            # длина каждой записи - от 1 до 100 чисел
             line_length = random.randint(1, 100)
             line = ""
             for _ in range(line_length):
+                # числа в диапазоне от -100 до 100
                 char = str(random.randint(-100, 100))
+                # каждое число может предваряться и оканчиваться каким-либо разделителем
                 line += random.choice(separators) + char + random.choice(separators)
             line = line.strip()
             f.write(line + "\n")
+    logging.debug(f"Конец {create_err_file.__name__}()")
 
 
-err_file = "err_datasets.txt"
-create_err_file(err_file)
-
-
-def check_lines_against_regex(read_file, write_file):
+def check_lines(read_file, write_file):
+    logging.debug(f"Начало {check_lines.__name__}()")
     with open(read_file, "r") as read_f:
         with open(write_file, 'w') as write_f:
             for _, line in enumerate(read_f, start=1):
                 line = line.strip()
+                logging.debug(line)
                 # заменяем в строке line все вхождения символов, отличных от 0-9,
                 # символа '-' для отрицательных чисел, а также разрешённого разделителя ','
                 # на разделитель ','
                 line = re.compile(r'[^0-9-,]').sub(",", line)
+                logging.debug(line)
                 # возможно образовались пары разделителей ',,'
                 # заменим их на одно вхождение ','
                 line = re.compile(r'(,,)').sub(",", line)
+                logging.debug(line)
                 # уберём ',' в начале и в конце строки
                 line = re.compile(r'(^,)|(,$)').sub("", line)
+                logging.debug(line)
+                logging.debug("---")
                 write_f.write(line + "\n")
+    logging.debug(f"Конец {check_lines.__name__}()")
 
 
-norm_datasets = "norm_datasets.txt"
-check_lines_against_regex(err_file, norm_datasets)
+def main():
+    logging.debug(f"Начало {main.__name__}()")
+    err_file = "err_datasets.txt"
+    create_err_file(err_file)
+    norm_datasets = "norm_datasets.txt"
+    check_lines(err_file, norm_datasets)
+    logging.debug(f"Конец {main.__name__}()")
+
+
+if __name__ == "__main__":
+    logging.debug(f"Начало работы скрипта {os.path.basename(__file__)}")
+    main()
+    logging.debug(f"Конец работы скрипта {os.path.basename(__file__)}")
