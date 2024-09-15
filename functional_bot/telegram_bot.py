9bc73d3fa74bfb79cfe65bb72d6f1a89db9d26ce
AleksandrBelous <ab21082001@mail.ru>
2024-09-15 20:20:40 +0300

    -

--- functional_bot/telegram_bot.py
+++ functional_bot/telegram_bot.py
@@ -1,4 +1,3 @@
-import requests
 import re
 import os
 import datetime
@@ -12,13 +11,20 @@ if logging.getLogger().isEnabledFor(logging.CRITICAL):
                         format=' %(asctime)s - %(levelname)s - %(message)s'
                         )
 
+logger = logging.getLogger(__name__)
+
 from dotenv import load_dotenv
 from pathlib import Path
 
-from telegram import Update, ForceReply
+from telegram import Update, ForceReply, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 
-logger = logging.getLogger(__name__)
+
+class DotDict(dict):
+    """Позволяет обращаться к элементам словаря через точку."""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
 
 
 class TelegramBot:
@@ -26,25 +32,99 @@ class TelegramBot:
         dotenv_path = Path('.env')
         load_dotenv(dotenv_path=dotenv_path)
 
-        self.tm_token = os.getenv('TM_TOKEN')
+        self.tm_token__ = os.getenv('TM_TOKEN')
+        self.chat_id = os.getenv('CHAT_ID')
+        self.commands = DotDict(
+                {
+                        'start'           : DotDict(
+                                {
+                                        'command'    : 'start',
+                                        'button'     : '/start',
+                                        'state_point': None,
+                                        'callback'   : self.command_Start,
+                                        }
+                                ),
+                        'help'            : DotDict(
+                                {
+                                        'command'    : 'help',
+                                        'button'     : '/help',
+                                        'state_point': None,
+                                        'callback'   : self.command_Help,
+                                        }
+                                ),
+                        'echo'            : DotDict(
+                                {
+                                        'command'    : 'echo',
+                                        'button'     : '/echo',
+                                        'state_point': None,
+                                        'callback'   : self.command_Echo,
+                                        }
+                                ),
+                        'findPhoneNumbers': DotDict(
+                                {
+                                        'command'    : 'findPhoneNumbers',
+                                        'button'     : '/findPhoneNumbers',
+                                        'state_point': 'findPhoneNumbers',
+                                        'callback'   : self.command_FindPhoneNumbers,
+                                        }
+                                ),
+                        'cancel'          : DotDict(
+                                {
+                                        'command'    : 'cancel',
+                                        'button'     : '/cancel',
+                                        'state_point': None,
+                                        'callback'   : self.command_Cancel,
+                                        }
+                                ),
+                        },
+                )
 
-    @staticmethod
-    def start(update: Update, context):
-        user = update.effective_user
-        update.message.reply_text(f'Привет {user.full_name}!')
+    # Функция для создания кнопок
+    def keyboard_menu_main(self):
+        return ReplyKeyboardMarkup([
+                [KeyboardButton(self.commands.start.button)],
+                [KeyboardButton(self.commands.help.button)],
+                [KeyboardButton(self.commands.findPhoneNumbers.button)],
+                ], resize_keyboard=True
+                )
 
-    @staticmethod
-    def helpCommand(update: Update, context):
-        update.message.reply_text('Help!')
+    def keyboard_menu_cancel(self):
+        return ReplyKeyboardMarkup([
+                [KeyboardButton(self.commands.cancel.button)],
+                ], resize_keyboard=True
+                )
 
-    @staticmethod
-    def findPhoneNumbersCommand(update: Update, context):
-        update.message.reply_text('Введите текст для поиска телефонных номеров: ')
+    def command_Start(self, update: Update = None, context=None):
+        if update:
+            user = update.effective_user
+            update.message.reply_text(
+                    f'Привет {user.full_name}!',
+                    reply_markup=self.keyboard_menu_main()  # Отправляем клавиатуру с кнопками
+                    )
+        else:
+            context.bot.send_message(
+                    chat_id=self.chat_id,
+                    text="Бот запущен! Вот ваша стартовая кнопка:",
+                    reply_markup=self.keyboard_menu_main()
+                    )
 
-        return 'findPhoneNumbers'
+    def command_Help(self, update: Update, context):
+        update.message.reply_text('Help!', reply_markup=self.keyboard_menu_main())
 
-    @staticmethod
-    def findPhoneNumbers(update: Update, context):
+    def command_FindPhoneNumbers(self, update: Update, context):
+        update.message.reply_text('Введите текст для поиска телефонных номеров: ',
+                                  # reply_markup=self.main_menu_keyboard()
+                                  reply_markup=self.keyboard_menu_cancel()
+                                  # Кнопка для отмены поиска
+                                  )
+
+        return self.commands.findPhoneNumbers.state_point
+
+    def command_Cancel(self, update: Update, context):
+        update.message.reply_text('Запрос отменен.', reply_markup=self.keyboard_menu_main())
+        return ConversationHandler.END
+
+    def findPhoneNumbers(self, update: Update, context):
         user_input = update.message.text  # Получаем текст, содержащий (или нет) номера телефонов
 
         phoneNumRegex = re.compile(r'8 \(\d{3}\) \d{3}-\d{2}-\d{2}')  # формат 8 (000) 000-00-00
@@ -52,7 +132,7 @@ class TelegramBot:
         phoneNumberList = phoneNumRegex.findall(user_input)  # Ищем номера телефонов
 
         if not phoneNumberList:  # Обрабатываем случай, когда номеров телефонов нет
-            update.message.reply_text('Телефонные номера не найдены')
+            update.message.reply_text('Телефонные номера не найдены', reply_markup=self.keyboard_menu_cancel())
             return  # Завершаем выполнение функции
 
         phoneNumbers = ''  # Создаем строку, в которую будем записывать номера телефонов
@@ -62,34 +142,44 @@ class TelegramBot:
         update.message.reply_text(phoneNumbers)  # Отправляем сообщение пользователю
         return ConversationHandler.END  # Завершаем работу обработчика диалога
 
-    @staticmethod
-    def echo(update: Update, context):
-        update.message.reply_text(update.message.text)
+    def command_Echo(self, update: Update, context):
+        logger.debug(f'start {self.command_Echo.__name__}')
+        # print('in echo')
+        update.message.reply_text(update.message.text, reply_markup=self.keyboard_menu_main())
 
     def main(self):
-        updater = Updater(self.tm_token, use_context=True)
+        updater = Updater(self.tm_token__, use_context=True)
 
         # Получаем диспетчер для регистрации обработчиков
         dp = updater.dispatcher
-        # Обработчик диалога
-        convHandlerFindPhoneNumbers = ConversationHandler(
-                entry_points=[CommandHandler('findPhoneNumbers', self.findPhoneNumbersCommand)],
-                states={
-                        'findPhoneNumbers': [MessageHandler(Filters.text & ~Filters.command, self.findPhoneNumbers)],
-                        },
-                fallbacks=[]
-                )
 
         # Регистрируем обработчики команд
-        dp.add_handler(CommandHandler("start", self.start))
-        dp.add_handler(CommandHandler("help", self.helpCommand))
-        dp.add_handler(convHandlerFindPhoneNumbers)
+
+        # Обработчик команды /start
+        dp.add_handler(CommandHandler(self.commands.start.command, self.commands.start.callback))
+        # Обработчик команды /help
+        dp.add_handler(CommandHandler(self.commands.help.command, self.commands.help.callback))
+        # Обработчик команды /findPhoneNumbers
+        dp.add_handler(ConversationHandler(
+                entry_points=[CommandHandler(self.commands.findPhoneNumbers.state_point,
+                                             self.commands.findPhoneNumbers.callback
+                                             )],
+                states={
+                        self.commands.findPhoneNumbers.state_point: [
+                                MessageHandler(Filters.text & ~Filters.command, self.findPhoneNumbers)],
+                        },
+                fallbacks=[CommandHandler(self.commands.cancel.command, self.commands.cancel.callback)]
+                )
+                )
 
         # Регистрируем обработчик текстовых сообщений
-        dp.add_handler(MessageHandler(Filters.text & ~Filters.command, self.echo))
+        dp.add_handler(MessageHandler(Filters.text & ~Filters.command, self.commands.echo.callback))
 
         # Запускаем бота
         updater.start_polling()
+
+        # Отправляем кнопку автоматически при запуске бота
+        self.command_Start(context=updater)
 
         # Останавливаем бота при нажатии Ctrl+C
         updater.idle()
