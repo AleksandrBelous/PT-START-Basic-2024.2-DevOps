ae3322137a8305262a57c0fd8116e494c4de709a
AleksandrBelous <ab21082001@mail.ru>
2024-09-15 21:14:28 +0300

    -

--- functional_bot/telegram_bot.py
+++ functional_bot/telegram_bot.py
@@ -48,6 +48,14 @@ class TelegramBot:
                                         'callback'   : self.command_Start,
                                         }
                                 ),
+                        'cancel'          : DotDict(
+                                {
+                                        'command'    : 'cancel',
+                                        'button'     : '/cancel',
+                                        'state_point': None,
+                                        'callback'   : self.command_Cancel,
+                                        }
+                                ),
                         'help'            : DotDict(
                                 {
                                         'command'    : 'help',
@@ -64,27 +72,163 @@ class TelegramBot:
                                         'callback'   : self.command_Echo,
                                         }
                                 ),
+
+                        ### 1. Поиск информации в тексте и её вывод.
+                        'findEmails'      : DotDict(
+                                {
+                                        'command'    : 'find_email',
+                                        'button'     : '/find_email',
+                                        'state_point': 'find_email',
+                                        'callback'   : self.command_FindEmails,
+                                        }
+                                ),
                         'findPhoneNumbers': DotDict(
                                 {
-                                        'command'    : 'findPhoneNumbers',
-                                        'button'     : '/findPhoneNumbers',
-                                        'state_point': 'findPhoneNumbers',
+                                        'command'    : 'find_phone_number',
+                                        'button'     : '/find_phone_number',
+                                        'state_point': 'find_phone_number',
                                         'callback'   : self.command_FindPhoneNumbers,
                                         }
                                 ),
-                        'cancel'          : DotDict(
+
+                        ### 2. Проверка сложности пароля регулярным выражением.
+                        'verifyPassword'  : DotDict(
                                 {
-                                        'command'    : 'cancel',
-                                        'button'     : '/cancel',
-                                        'state_point': None,
-                                        'callback'   : self.command_Cancel,
+                                        'command'    : 'verify_password',
+                                        'button'     : '/verify_password',
+                                        'state_point': 'verify_password',
+                                        'callback'   : self.command_VerifyPassword,
+                                        }
+                                ),
+                        ### 3. Мониторинг Linux-системы.
+
+                        ## 3.1. Сбор информации о системе.
+
+                        # 3.1.1. О релизе.
+                        'getRelease'      : DotDict(
+                                {
+                                        'command'    : 'get_release',
+                                        'button'     : '/get_release',
+                                        'state_point': 'get_release',
+                                        'callback'   : self.command_GetRelease,
+                                        }
+                                ),
+
+                        # 3.1.2. Об архитектуры процессора, имени хоста системы и версии ядра.
+                        'getUname'        : DotDict(
+                                {
+                                        'command'    : 'get_uname',
+                                        'button'     : '/get_uname',
+                                        'state_point': 'get_uname',
+                                        'callback'   : self.command_GetUname,
+                                        }
+                                ),
+
+                        # 3.1.3. О времени работы.
+                        'getUptime'       : DotDict(
+                                {
+                                        'command'    : 'get_uptime',
+                                        'button'     : '/get_uptime',
+                                        'state_point': 'get_uptime',
+                                        'callback'   : self.command_GetUptime,
+                                        }
+                                ),
+
+                        ## 3.2. Сбор информации о состоянии файловой системы.
+                        'getDF'           : DotDict(
+                                {
+                                        'command'    : 'get_df',
+                                        'button'     : '/get_df',
+                                        'state_point': 'get_df',
+                                        'callback'   : self.command_GetDF,
+                                        }
+                                ),
+
+                        ## 3.3. Сбор информации о состоянии оперативной памяти.
+                        'getFree'         : DotDict(
+                                {
+                                        'command'    : 'get_free',
+                                        'button'     : '/get_free',
+                                        'state_point': 'get_free',
+                                        'callback'   : self.command_GetFree,
+                                        }
+                                ),
+
+                        ## 3.4. Сбор информации о производительности системы.
+                        'getMpstat'       : DotDict(
+                                {
+                                        'command'    : 'get_mpstat',
+                                        'button'     : '/get_mpstat',
+                                        'state_point': 'get_mpstat',
+                                        'callback'   : self.command_GetMpstat,
+                                        }
+                                ),
+
+                        ## 3.5. Сбор информации о работающих в данной системе пользователях.
+                        'getW'            : DotDict(
+                                {
+                                        'command'    : 'get_w',
+                                        'button'     : '/get_w',
+                                        'state_point': 'get_w',
+                                        'callback'   : self.command_GetW,
+                                        }
+                                ),
+
+                        ## 3.6. Сбор логов.
+
+                        # 3.6.1. Последние 10 входов в систему.
+                        'getAuths'        : DotDict(
+                                {
+                                        'command'    : 'get_auths',
+                                        'button'     : '/get_auths',
+                                        'state_point': 'get_auths',
+                                        'callback'   : self.command_GetAuths,
+                                        }
+                                ),
+                        # 3.6.2. Последние 5 критических событий.
+                        'getCritical'     : DotDict(
+                                {
+                                        'command'    : 'get_critical',
+                                        'button'     : '/get_critical',
+                                        'state_point': 'get_critical',
+                                        'callback'   : self.command_GetCritical,
+                                        }
+                                ),
+
+                        ## 3.7 Сбор информации о запущенных процессах.
+                        'gepPS'           : DotDict(
+                                {
+                                        'command'    : 'get_ps',
+                                        'button'     : '/get_ps',
+                                        'state_point': 'get_ps',
+                                        'callback'   : self.command_GetPS,
+                                        }
+                                ),
+
+                        ## 3.8 Сбор информации об используемых портах.
+                        'getSS'           : DotDict(
+                                {
+                                        'command'    : 'get_ss',
+                                        'button'     : '/get_ss',
+                                        'state_point': 'get_ss',
+                                        'callback'   : self.command_GetSS,
+                                        }
+                                ),
+
+                        ## 3.9 Сбор информации об установленных пакетах.
+                        'getAptList'      : DotDict(
+                                {
+                                        'command'    : 'get_apt_list',
+                                        'button'     : '/get_apt_list',
+                                        'state_point': 'get_apt_list',
+                                        'callback'   : self.command_GetAptList,
                                         }
                                 ),
                         },
                 )
         logger.info(f'Stop {self.__init__.__name__}')
 
-    # Функция для создания кнопок
+    # Функция для создания кнопок основных запросов
     def keyboard_menu_main(self):
         logger.info(f'Start {self.keyboard_menu_main.__name__}')
         return ReplyKeyboardMarkup([
@@ -94,6 +238,7 @@ class TelegramBot:
                 ], resize_keyboard=True
                 )
 
+    # Функция для создания кнопки отмены запроса
     def keyboard_menu_cancel(self):
         logger.info(f'Start {self.keyboard_menu_cancel.__name__}')
         return ReplyKeyboardMarkup([
@@ -112,7 +257,7 @@ class TelegramBot:
         else:
             context.bot.send_message(
                     chat_id=self.chat_id,
-                    text="Бот запущен! Вот ваша стартовая кнопка:",
+                    text="Бот запущен!",
                     reply_markup=self.keyboard_menu_main()
                     )
         logger.info(f'Stop {self.command_Start.__name__}')
@@ -122,23 +267,25 @@ class TelegramBot:
         update.message.reply_text('Help!', reply_markup=self.keyboard_menu_main())
         logger.info(f'Stop {self.command_Help.__name__}')
 
-    def command_FindPhoneNumbers(self, update: Update, context):
-        logger.info(f'Start {self.command_FindPhoneNumbers.__name__}')
-        update.message.reply_text('Введите текст для поиска телефонных номеров: ',
-                                  # reply_markup=self.main_menu_keyboard()
-                                  reply_markup=self.keyboard_menu_cancel()
-                                  # Кнопка для отмены поиска
-                                  )
-        logger.info(f'Stop {self.command_FindPhoneNumbers.__name__}')
-        return self.commands.findPhoneNumbers.state_point
-
     def command_Cancel(self, update: Update, context):
         logger.info(f'Start {self.command_Cancel.__name__}')
         update.message.reply_text('Запрос отменен.', reply_markup=self.keyboard_menu_main())
         logger.info(f'Stop {self.command_Cancel.__name__}')
         return ConversationHandler.END
 
+    def command_FindPhoneNumbers(self, update: Update, context):
+        logger.info(f'Start {self.command_FindPhoneNumbers.__name__}')
+        update.message.reply_text('Введите текст для поиска телефонных номеров: ',
+                                  reply_markup=self.keyboard_menu_cancel()
+                                  # Кнопка для отмены поиска
+                                  )
+        logger.info(f'Stop {self.command_FindPhoneNumbers.__name__}')
+        return self.commands.findPhoneNumbers.state_point
+
     def findPhoneNumbers(self, update: Update, context):
+        """
+        Бот вывод список найденных номеров телефона или email-адресов
+        """
         logger.info(f'Start {self.findPhoneNumbers.__name__}')
         user_input = update.message.text  # Получаем текст, содержащий (или нет) номера телефонов
 
@@ -165,12 +312,13 @@ class TelegramBot:
 
     def main(self):
         logger.info(f'Start {self.main.__name__}')
+
         updater = Updater(self.tm_token__, use_context=True)
 
         # Получаем диспетчер для регистрации обработчиков
         dp = updater.dispatcher
 
-        ### Регистрируем обработчики команд ###
+        ## Регистрируем обработчики команд
 
         # Обработчик команды /start
         dp.add_handler(CommandHandler(self.commands.start.command, self.commands.start.callback))
@@ -200,6 +348,7 @@ class TelegramBot:
 
         # Останавливаем бота при нажатии Ctrl+C
         updater.idle()
+
         logger.info(f'Stop {self.main.__name__}')
 
 
