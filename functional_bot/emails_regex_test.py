f52d77351cf04cfa1fa62b4b7e15f746a2375483
AleksandrBelous <ab21082001@mail.ru>
2024-09-20 10:44:53 +0300

    -

--- functional_bot/emails_regex_test.py
+++ functional_bot/emails_regex_test.py
@@ -7,7 +7,7 @@ text = ("kolbeev@example.compoiuytrewq,./;[]=-0987654321~!@#$%^&*()"
         "user123@gmail.compoiuytrewq,./;[]=-0987654321~!@#$%^&*()"
         "test@yandex.ru.|}{:?><,./;[]=-0987654321~!@#$%^&()_")
 
-emailsRegex = re.compile(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}')  # формат
+emailsRegex = re.compile(r'[a-zA-Z0-9.]+@[a-zA-Z0-9.]+\.[a-zA-Z]{2,3}')
 
 emailsList = emailsRegex.findall(text)
 print(*emailsList, sep='\n')
