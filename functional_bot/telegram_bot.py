f3038ac266bd114b6d66928968d14656e7ae3177
AleksandrBelous <ab21082001@mail.ru>
2024-09-22 22:39:17 +0300

    -

--- functional_bot/telegram_bot.py
+++ functional_bot/telegram_bot.py
@@ -666,7 +666,7 @@ class TelegramBot:
             try:
                 logger.info(line)
                 template = re.compile(
-                        fr'^({year})'
+                        fr'^({year})-({month})-({day})'
                         )
                 line = template.search(line)
                 logging.info(line.groups())
