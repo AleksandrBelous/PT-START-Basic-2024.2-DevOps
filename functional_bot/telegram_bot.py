9e70541f538eae706a62d01be13d2cbaf1294199
AleksandrBelous <ab21082001@mail.ru>
2024-09-23 20:15:25 +0300

    -

--- functional_bot/telegram_bot.py
+++ functional_bot/telegram_bot.py
@@ -494,7 +494,7 @@ class TelegramBot:
                 connection.close()
                 logging.info("Соединение с PostgreSQL закрыто")
         logger.info(f'Stop {self.command_Add_db_Emails.__name__}')
-        return ConversationHandler.END
+        return  # ConversationHandler.END
 
     def command_FindPhoneNumbers(self, update: Update, context):
         """
@@ -572,7 +572,7 @@ class TelegramBot:
                 connection.close()
                 logging.info("Соединение с PostgreSQL закрыто")
         logger.info(f'Stop {self.command_Add_db_Phones.__name__}')
-        return ConversationHandler.END
+        return  # ConversationHandler.END
 
     def command_VerifyPassword(self, update: Update, context):
         """
